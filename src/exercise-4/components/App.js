76d20fa8c6880916d01cf61c6b166ce88314d51e
syq <1035394794@qq.com>
2020-09-23 22:59:42 +0800

    [syq] complete exercise 4

--- src/exercise-4/components/App.js
+++ src/exercise-4/components/App.js
@@ -1,20 +1,27 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Route} from "react-router";
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router";
 import Home from "./Home";
 import About from "./About";
 import User from "./User";
 import NotMatch from "./NotMatch";
 
 class App extends Component {
+  routerWillLeave(nextLocation) {
+    if (!nextLocation.pathname.match("^[0-9]*$")) {
+      this.props.history.push("/a/b");
+    }
+  }
   render() {
     return (
       <div className="app">
         <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/:user(\d+)" component={User} />
             <Route component={NotMatch} />
-            <Route path='/' component={Home} />
-            <Route path='/:user' component={User} />
-            <Route path='/about' component={About} />
+          </Switch>
         </Router>
       </div>
     );
