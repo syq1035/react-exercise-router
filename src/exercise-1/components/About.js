e38e12396fea8ea73ac7d7c50f5116d35b3bff40
syq <1035394794@qq.com>
2020-09-23 20:59:13 +0800

    [syq] complete exercise 1

---
+++ src/exercise-1/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <p>Company: ThoughtWorks Local </p>
+      <p>Location: Xi'an </p>
+      <p>
+        For more information, please view our <a href="/">website</a>.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,12 +1,25 @@
-import React, {Component} from 'react';
-import '../styles/App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
-
+import React, { Component } from "react";
+import "../styles/App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router";
+import Home from "./Home";
+import About from "./About";
+import MyProfile from "./MyProfile";
+import Header from "./Header";
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Router />
+        <Router>
+          <Header />
+          <div className="main">
+            <Switch>
+              <Route exact path="/myProfile" component={MyProfile} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
